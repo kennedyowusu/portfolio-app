d06fe6c52d921a217c402e661af6b4ff33fcbf81
Kennedy Owusu <kennediowusu@gmail.com>
2023-06-27 20:20:54 +0000

    add motions to nav components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import useMediaQuery from "./hooks/useMediaQuery"
 import Navbar from "./components/Navbar";
 import DotGroup from "./components/DotGroup";
 import HeroSection from "./components/HeroSection";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 import Divider from "./components/Divider";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
@@ -43,23 +43,53 @@ function App() {
             <DotGroup selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
           )
         }
-        <HeroSection setIsTopOfPage={setSelectedPage} />
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("home")}
+        >
+          <HeroSection setIsTopOfPage={setSelectedPage} />
+        </motion.div>
       </div>
       <Divider />
       <div className="w-5/6 mx-auto md:h-full ">
-        <Skills />
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("skills")}
+        >
+          <Skills />
+        </motion.div>
       </div>
       <Divider />
       <div className="w-5/6 mx-auto">
-        <Projects />
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("projects")}
+        >
+          <Projects />
+        </motion.div>
       </div>
       <Divider />
       <div className="w-5/6 mx-auto md:h-full">
-        <Testimonials />
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("testimonials")}
+        >
+          <Testimonials />
+        </motion.div>
       </div>
       <Divider />
       <div className="w-5/6 mx-auto md:h-full">
-        <Contact />
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("contact")}
+        >
+          <Contact />
+        </motion.div>
       </div>
       <Footer />
     </div>
